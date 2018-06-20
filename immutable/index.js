6fa0a88931be56931e02f877a1652697c515d692
Eugene Niamarsky <niamarsky@gmail.com>
2018-06-20 13:37:21 +0300

    Add changed version for jsan library
    When we work with big object where there is the equal object we takes wrong data (undefined), becose $jsan path not resolved in Immutable object

--- immutable/index.js
+++ immutable/index.js
@@ -4,7 +4,19 @@ var serialize = require('./serialize');
 module.exports = function(Immutable, refs) {
   return {
     stringify: function(data) {
-      return jsan.stringify(data, serialize(Immutable, refs).replacer, null, true);
+      return jsan.stringify(data, serialize(Immutable, refs).replacer, null, {
+          'date': true,
+          'function': true,
+          'regex': true,
+          'undefined': true,
+          'error': true,
+          'symbol': true,
+          'map': true,
+          'set': true,
+          'nan': true,
+          'infinity': true,
+          'searchIdenticalObjects': false,
+      });
     },
     parse: function(data) {
       return jsan.parse(data, serialize(Immutable, refs).reviver);
